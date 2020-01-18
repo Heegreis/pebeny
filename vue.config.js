4c3f186ef8576963d82bb1bf7e8d236f0819514e
Heegreis <run3113214@gmail.com>
2020-01-18 16:27:05 +0800

    fix vuetify config error and set building config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,36 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        // options placed here will be merged with default configuration and passed to electron-builder
+        "appId": "com.informagine.pebeny",
+        "productName": "Pebeny",
+        "directories": {
+          "output": "./dist"
+        },
+        "win": {
+          "target": [
+            {
+              "target": "nsis",
+              "arch": [
+                "x64"
+              ]
+            }
+          ]
+        },
+        "nsis": {
+          "oneClick": false, // 是否一键安装
+          "allowElevation": true, // 允许请求提升。 如果为false，则用户必须使用提升的权限重新启动安装程序。
+          "allowToChangeInstallationDirectory": true, // 允许修改安装目录
+          "installerHeaderIcon": "./public/favicon.ico", // 安装时头部图标
+          "createDesktopShortcut": true, // 创建桌面图标
+          "shortcutName": "Pebeny", // 图标名称
+          "unicode": true,
+        },
+      }
+    }
+  }
 }
